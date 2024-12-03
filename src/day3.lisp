2a66a87f237c83aa94ea5f39e6eefec1ec0cb8ab
NMunro <neilmunro@gmail.com>
2024-12-03 09:30:59 +0000

    Made start on part 2

--- src/day3.lisp
+++ src/day3.lisp
@@ -11,7 +11,17 @@
     (let ((nums (cl-utilities:split-sequence #\, (string-trim '(#\( #\)) (subseq mul 3)))))
         (apply #'* (mapcar #'parse-integer nums))))
 
+(defun part-1 (data)
+  (let ((muls (ppcre:all-matches-as-strings "mul\\([0-9]{1,3},[0-9]{1,3}\\)" data)))
+    (apply #'+ (mapcar #'process-mul-1 muls))))
+
+(defun part-2 (data)
+    (let* ((matches (ppcre:all-matches-as-strings "(?<=do\\(\\))(.*?)(?=don't\\(\\)|$)" data)))
+        (apply #'+ (loop :for match :in matches :collect (part-1 match)))))
+
 (defun day3 (file)
-  (let ((data (load-data file)))
-    (let ((muls (ppcre:all-matches-as-strings "mul\\([0-9]{1,3},[0-9]{1,3}\\)" data)))
-      (list (apply #'+ (mapcar #'process-mul muls)) nil))))
+    (list (part-1 (load-data file)) (part-2 (load-data file))))
+
+(let* ((data (load-data #p"~/quicklisp/local-projects/aoc-2024/data/day3-data.txt"))
+       (matches (ppcre:all-matches-as-strings "(?<=do\\(\\))(.*?)(?=don't\\(\\)|$)")))
+  (format t "~A~%" matches))
