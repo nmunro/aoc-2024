be33f4569e506272d996957bf3fc6cb17666a7d6
NMunro <neilmunro@gmail.com>
2024-12-03 22:32:35 +0000

    Day 3, part 2

--- src/day3.lisp
+++ src/day3.lisp
@@ -16,10 +16,13 @@
     (apply #'+ (mapcar #'process-mul muls))))
 
 (defun part-2 (data)
-  (let ((matches (ppcre:all-matches-as-strings "mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)" data)))
-    (dolist (match matches)
-      (format t "Match: ~A~%" match))
-    (apply #'+ (mapcar #'part-1 matches))))
+    (loop :with muls = '()
+          :with process = t
+          :for match :in (ppcre:all-matches-as-strings "mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)" data)
+          :do (cond ((string-equal match "do()") (setf process t))
+                    ((string-equal match "don't()") (setf process nil))
+                    (process (push match muls)))
+          :finally (return (apply #'+ (mapcar #'part-1 muls)))))
 
 (defun day3 (file)
     (list (part-1 (load-data file)) (part-2 (load-data file))))
