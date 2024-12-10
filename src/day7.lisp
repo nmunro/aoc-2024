71b033d7aa9d743fcfa4c8cfdc9c709cddc4ad35
Neil Munro <neil@elixirsoftware.co.uk>
2024-12-10 00:30:04 +0000

    Day 7 done

--- src/day7.lisp
+++ src/day7.lisp
@@ -4,20 +4,37 @@
 
 (in-package advent-of-code-2024/day7)
 
+(defun || (a b)
+  (parse-integer (format nil "~A~A" a b)))
+
 (defun load-data (file)
   (uiop:read-file-lines file))
 
-(defun process-permutations (data)
-  (loop :for perm :in (permutations '(+ *) (1- (length data)))
-        :collect (combine-data-and-permutations data perm)))
+(defun evaluate-left-to-right (expr)
+  (reduce (lambda (accum next)
+            (let ((operator (first next))
+                  (operand (second next)))
+              (if (numberp accum)
+                  (funcall operator accum operand)
+                  (funcall (first accum) (second accum) (first next)))))
+          (pair-operators-and-operands expr)
+          :initial-value (first expr)))
 
-(defun process-line (line)
-    (let ((data (str:split ": " line)))
-        (list :total (parse-integer (car data))
-              :data (process-permutations (mapcar #'parse-integer (coerce (str:split " " (cadr data)) 'list))))))
+(defun pair-operators-and-operands (expr)
+  (loop for i from 1 by 2 to (1- (length expr))
+        collect (list (nth i expr) (nth (1+ i) expr))))
 
-(defun process-data (data)
-  (mapcar (lambda (line) (process-line line)) data))
+(defun process-permutations (data permutations)
+  (loop :for perm :in (permutations permutations (1- (length data)))
+        :collect (evaluate-left-to-right (combine-data-and-permutations data perm))))
+
+(defun process-line (line permutations)
+    (let* ((data (str:split ": " line))
+           (total (parse-integer (car data))))
+        (member total (process-permutations (mapcar #'parse-integer (coerce (str:split " " (cadr data)) 'list)) permutations))))
+
+(defun process-data (data permutations)
+  (remove nil (mapcar (lambda (line) (process-line line permutations)) data)))
 
 (defun permutations (l count)
   (if (= count 1)
@@ -37,12 +54,12 @@
   (apply #'+ totals))
 
 (defun part-1 (data)
-    (process-data data))
+    (sum-totals (loop :for pd :in (process-data data '(+ *)) :collect (car pd))))
 
 (defun part-2 (data)
-    nil)
+    (sum-totals (loop :for pd :in (process-data data '(+ * ||)) :collect (car pd))))
 
 (defun day7 (file)
-  (list (part-1 (load-data file)) (part-2 (load-data file))))
+  (format t "~A~% "(list (part-1 (load-data file)) (part-2 (load-data file)))))
 
-(part-1 (load-data #p"~/quicklisp/local-projects/aoc-2024/data/day7-demo-data.txt"))
+(day7 #p"~/quicklisp/local-projects/aoc-2024/data/day7-data.txt")
