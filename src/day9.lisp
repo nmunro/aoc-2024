014645c5e84c8224bef452d8ed727fbc93743309
Neil Munro <neil@elixirsoftware.co.uk>
2024-12-13 12:03:13 +0000

    Ground work for day 9 part 2

--- src/day9.lisp
+++ src/day9.lisp
@@ -1,21 +1,9 @@
 (defpackage advent-of-code-2024/day9
   (:use :cl)
-  (:import-from :advent-of-code-2024/utils #:swap-elements)
+  (:import-from :advent-of-code-2024/utils #:swap-elements #:flatten*)
   (:export #:day9))
 
 (in-package advent-of-code-2024/day9)
-
-(defun flatten* (lst depth &aux (re '()))
-  (cond
-    ((null lst) '())  ;; If list is empty, return empty list
-    ((listp (car lst))
-     (append (cond
-               ((= 0 depth) (list (car lst)))  ;; Don't flatten
-               ((< 0 depth) (flatten* (car lst) (- depth 1)))  ;; Flatten down
-               ((= -1 depth) (flatten* (car lst) depth))  ;; Flatten all
-               ((< depth -1) (list (flatten* (car lst) (+ depth 1)))))  ;; Flatten up
-             (flatten* (cdr lst) depth)))
-    (t (cons (car lst) (flatten* (cdr lst) depth)))))
 
 (defun create-diskmap (map)
   (flet ((generate-file-size-block (num file-id) (loop :for x :from 0 :to (1- num) :collect file-id))
@@ -44,33 +32,42 @@
         :until (not (and (stringp char) (string= char ".")))
         :finally (return idx)))
 
-(defun is-sorted-p (diskmap index)
-  (loop :for i :from index :to (1- (length diskmap))
-        :always (and (stringp (elt diskmap i)) (string= (elt diskmap i) "."))))
-
-(defun defrag (diskmap)
-  (dotimes (x (length diskmap))
-    (when (is-sorted-p diskmap x)
+(defun defrag-1 (diskmap)
+  (flet ((is-sorted-p (diskmap index)
+           (loop :for i :from index :to (1- (length diskmap))
+                 :always (and (stringp (elt diskmap i)) (string= (elt diskmap i) ".")))))
+    (dotimes (x (length diskmap))
+      (when (is-sorted-p diskmap x)
         (return diskmap))
-    (let ((char (elt diskmap x)))
-      (when (and (stringp char) (string= char "."))
-        (swap-elements diskmap x (find-last-file-block diskmap)))))
-  diskmap)
+      (let ((char (elt diskmap x)))
+        (when (and (stringp char) (string= char "."))
+          (swap-elements diskmap x (find-last-file-block diskmap)))))
+    diskmap))
 
-(defun calculate-checksum (diskmap)
+(defun defrag-2 (diskmap)
+  nil)
+
+(defun calculate-checksum-1 (diskmap)
   (let ((data (subseq diskmap 0 (position "." diskmap :test #'equal))))
     (loop :for num :across data
           :for count :from 0 :to (1- (length data))
           :sum (* num count))))
 
+(defun calculate-checksum-2 (diskmap)
+  nil)
+
 (defun part-1 (diskmap)
   (let ((copied-diskmap (copy-seq diskmap)))
-    (defrag copied-diskmap)
-    (calculate-checksum copied-diskmap)))
+    (defrag-1 copied-diskmap)
+    (calculate-checksum-1 copied-diskmap)))
 
-(defun part-2 (data)
-  nil)
+(defun part-2 (diskmap)
+  (let ((copied-diskmap (copy-seq diskmap)))
+    copied-diskmap))
 
 (defun day9 (path)
   (let ((data (load-data path)))
     (list (part-1 data) (part-2 data))))
+
+(let ((data (load-data #p"~/quicklisp/local-projects/aoc-2024/data/day9-demo-data.txt")))
+  (part-1 data))
